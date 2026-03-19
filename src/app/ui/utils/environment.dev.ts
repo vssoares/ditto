521136824e43688775435499dc480777d8123361
Vaqueiro <vsss23@hotmail.com>
2026-03-19 17:48:04 -0300

    v1.0.9 - alterando questoes de build

--- src/app/ui/utils/environment.dev.ts
+++ src/app/ui/utils/environment.dev.ts
@@ -1,3 +1,4 @@
 export const environment = {
   backendUrl: 'http://127.0.0.1:3000',
 };
+
---
+++ src/app/ui/utils/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  backendUrl: 'http://scssgw80csoo8wg440ks8csg.86.48.22.217.sslip.io',
+};
+
