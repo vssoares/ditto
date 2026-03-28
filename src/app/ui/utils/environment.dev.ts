b1e7e192b0ce270da6c060a38bd965abf976463b
Vinicius Soares <vsss23@hotmail.com>
2026-03-28 15:39:51 -0300

    fix: update baseHref for proper routing and adjust backend URL for development environment

--- src/app/ui/utils/environment.dev.ts
+++ src/app/ui/utils/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  // backendUrl: 'http://127.0.0.1:3000',
-  backendUrl: 'https://api.dittoapp.com.br',
+  backendUrl: 'http://127.0.0.1:3000',
+  // backendUrl: 'https://api.dittoapp.com.br',
 };
 
